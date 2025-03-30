b99676371041a6455d9e3d2c44b1977d3e24189e
Maxmuscoin <healingnolon@gmail.com>
2025-03-30 17:48:02 +0700

    Update MaxmusCoin.sol

--- contracts/MaxmusCoin.sol
+++ contracts/MaxmusCoin.sol
@@ -1,51 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MaxmusCoin {
-    string public name = "MaxmusCoin";
-    string public symbol = "MMC";
+contract MaxCoin {
+    string public name = "MaxCoin";
+    string public symbol = "MAX";
     uint8 public decimals = 18;
-    uint256 public totalSupply;
-
+    uint256 public totalSupply = 1000000000 * 10**uint256(decimals);
+    
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    constructor(uint256 _initialSupply) {
-        totalSupply = _initialSupply * 10 ** uint256(decimals);
-        balanceOf[msg.sender] = totalSupply;
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;  // Give all the tokens to the contract deployer
     }
 
-    function transfer(address to, uint256 amount) public returns (bool) {
-        require(to != address(0), "Invalid address");
-        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(balanceOf[msg.sender] >= value, "ERC20: insufficient balance");
 
-        balanceOf[msg.sender] -= amount;
-        balanceOf[to] += amount;
-
-        emit Transfer(msg.sender, to, amount);
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
         return true;
     }
 
-    function approve(address spender, uint256 amount) public returns (bool) {
-        allowance[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
+    function approve(address spender, uint256 value) public returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
         return true;
     }
 
-    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
-        require(from != address(0), "Invalid address");
-        require(to != address(0), "Invalid address");
-        require(balanceOf[from] >= amount, "Insufficient balance");
-        require(allowance[from][msg.sender] >= amount, "Allowance exceeded");
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(balanceOf[from] >= value, "ERC20: insufficient balance");
+        require(allowance[from][msg.sender] >= value, "ERC20: allowance exceeded");
 
-        balanceOf[from] -= amount;
-        balanceOf[to] += amount;
-        allowance[from][msg.sender] -= amount;
-
-        emit Transfer(from, to, amount);
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        allowance[from][msg.sender] -= value;
+        emit Transfer(from, to, value);
         return true;
     }
 }
